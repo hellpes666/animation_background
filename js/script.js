cfa6e86c4904d809ee2a1d126ecc93058e178eaa
Hellpes <alexrui_hpow@mail.ru>
2023-11-06 22:41:56 +0300

    timeDuration change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function lines() {
-    let sizeW = Math.random() * 23;
+    let sizeW = Math.random() * 25;
     let duration = Math.random() * 3;
     let e = document.createElement("div");
     e.setAttribute("class", "circle");
     document.body.appendChild(e);
 
-    e.style.width = 12 + sizeW + "px";
+    e.style.width = sizeW + "px";
     e.style.left = Math.random() * +innerWidth + "px";
     e.style.animationDuration = 2 + duration + "s";
 
