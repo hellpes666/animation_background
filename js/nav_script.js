42e9ad618586ed0da45badf7b7f79e14cb9aba19
Hellpes <alexrui_hpow@mail.ru>
2023-11-08 06:56:05 +0300

    styles and saas + nav script

--- js/nav_script.js
+++ js/nav_script.js
@@ -1,1 +1,20 @@
-// сделать нажатие на nav__item добавление класса active
+// Home
+document.getElementById("my-modal").addEventListener("click", function(){
+    document.getElementById("my-modal").classList.remove("open")
+    
+})
+document.getElementById("btn__home").addEventListener("click",function() {
+    document.getElementById("my-modal").classList.add("open")
+    
+})
+
+// FAQ
+
+document.getElementById("my-modal-faq").addEventListener("click", function(){
+    document.getElementById("my-modal-faq").classList.remove("open")
+    
+})
+document.getElementById("btn__FAQ").addEventListener("click",function() {
+    document.getElementById("my-modal-faq").classList.add("open")
+    
+})
